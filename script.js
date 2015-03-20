d49d72b83ed5f4ed6bbd3aceaaca7b9291dd1388
Ethan Penn <empenn54@gmail.com>
2015-03-20 15:09:42 -0500

    Added the dropdown for the subHeader. Still needs some bug fixing.

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 $(document).ready(function(){
-	$('div').hover(function(){
-		$('#AlertHome').css('background-color', 'blue')
-		
-		$('#AlertHome').hover(function(){
-			
-		})
-		$('headerRegLog').hover(function()){
-			
-		)}
+	
+	var subHeaderHidden=1;
+	
+	/*$('#subHeader').hover(function(){
+		if(subHeaderHidden){
+			$('#subHeader').animate({top: '+=42px'}, 500);
+			subHeaderHidden=false;
+		}else{
+			$('#subHeader').animate({top: '-=42px'}, 500);
+			subHeaderHidden=true;
+		}
+		console.log(subHeaderHidden);
+	}) */
+	
+	$('#subHeader').mouseenter(function(){
+			subHeaderHidden=2;
 	})
+	$('#subHeader').mouseleave(function(){
+			subHeaderHidden=0;
+	})
+	
+	function update(){
+		if(subHeaderHidden===0){
+			$('#subHeader').animate({top: '-=40px'}, 500);
+			subHeaderHidden=1;
+		}
+		if(subHeaderHidden===2){
+			$('#subHeader').animate({top: '+=40px'}, 500);
+			subHeaderHidden=1;
+		}
+	}
+	
+	function run(){
+			requestAnimFrame(function(){
+				run();
+			});
+			update();
+	}
+	run();
 });
+window.requestAnimFrame = (function(){
+	return window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame||
+	window.mozRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	function(callback){
+		window.setTimeout(callback, 1000 / 60);
+		console.log("RequestAnimationFrame: Failed")
+	};
+})();
